3fac8381225d3fb6448e6750806172fb67c85d2f
Sale-Sangameshwar <salesangameshwar1@gmail.com>
2026-07-19 12:49:34 +0530

    fix: delay process.exit to ensure mongoose connection errors flush to stdout

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,8 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
-    process.exit(1);
+    console.error(error);
+    setTimeout(() => process.exit(1), 1000);
   }
 };
 
